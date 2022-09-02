b2784013f8e7c888c9c9ee6aff405d6137836689
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 10:15:58 -0300

    feat: create circleOfNumbers.js

---
+++ src/circleOfNumbers.js
@@ -0,0 +1,10 @@
+function solution(n, firstNumber) {
+    return (firstNumber + n / 2) % n;
+}
+
+module.exports = solution;
+
+// npm init -y
+// npm i jest-cli -g
+// package.json: "scripts": { "test": "jest" }
+// npm test circleOfNumbers
